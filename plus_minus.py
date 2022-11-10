6a4c9a9f4f36070bb2e2d926570c5ed45bee8338
Erick Carrillo <erickcarrillo2807@gmail.com>
2022-11-09 22:11:30 -0600

    ejercicios

--- plus_minus.py
+++ plus_minus.py
@@ -14,10 +14,28 @@ import sys
 
 def plusMinus(arr):
     # Write your code here
+    positive_values = 0 
+    negative_values = 0
+    zeros = 0
+    length_arr = len(arr)
+    for i in arr:
+        #print(i)
+        if i > 0:
+            positive_values += 1
+        elif i < 0:
+            negative_values += 1
+        else: 
+            zeros += 1
+    
+    print(positive_values/length_arr)
+    print(negative_values/length_arr)
+    print(zeros/length_arr)
+
 
 if __name__ == '__main__':
-    n = int(input().strip())
+    #n = int(input().strip())
 
-    arr = list(map(int, input().rstrip().split()))
-
+    #arr = list(map(int, input().rstrip().split()))
+    n = 6
+    arr = [-4,3,-9,0,4,1]
     plusMinus(arr)
